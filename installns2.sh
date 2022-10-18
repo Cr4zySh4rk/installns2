8397d344ba76c9c95b3a104c29925e2416b00b8e
Adithya Manjunath <adithya@Adithyas-MacBook-Air.local>
2022-10-18 10:22:46 +0530

    Update installns2.sh

--- installns2.sh
+++ installns2.sh
@@ -1,13 +1,9 @@
 #!/bin/sh
 echo "NS2 installation script created by Adithya Manjunath"
 sleep 5
+sudo apt update
 sudo apt-get install -f nam -y
-tar -xzvf ns-allinone-2.35.tar.gz
-cd ns-allinone-2.35
-sudo apt-get install -f build-essential autoconf automake libxmu-dev
-sudo apt-get install -f libxt-dev libxt6 libsm-dev libsm6 
-libice-dev libice6 libxmu-dev
-sudo ./install
+sudo apt-get install -f ns2 -y
 dpkg-query -s ns2 > /dev/null 2>&1
 case $? in
 0)
