cbc25f75bf379b7258bbeffb86fded4cedeffc63
Adithya Manjunath <adithya@Adithyas-MacBook-Air.local>
2022-10-18 09:25:17 +0530

    Update installns2.sh

--- installns2.sh
+++ installns2.sh
@@ -8,8 +8,6 @@ sudo apt-get install build-essential autoconf automake libxmu-dev
 sudo apt-get install -f build-essential libxt-dev libxt6 libsm-dev libsm6 
 libice-dev libice6 libxmu-dev
 sudo ./install
-cd ..
-sudo rm -rf ns-allinone-2.35*
 dpkg-query -s ns2 > /dev/null 2>&1
 case $? in
 0)
