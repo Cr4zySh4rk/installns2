8be378530310ab4444c309e4c9423a78ccd81f22
Adithya Manjunath <adithya@Adithyas-Air.lan>
2022-10-18 07:44:48 +0530

    installns2 v2

--- installns2.sh
+++ installns2.sh
@@ -3,12 +3,16 @@ echo "NS2 installation script created by Adithya Manjunath"
 sleep 5
 sudo apt-get install -f nam -y
 sudo apt-get install -f ns2 -y
-REQUIRED_PKG="ns2"
-PKG_OK=$(dpkg-query -W --showformat='${Status}\n' $REQUIRED_PKG|grep 
-"install o>
-if [""= "$PKG_OK"];then
-  echo "Could not install NS2, check logs for more info."
-else
-  echo "NS2 has been installed successfully!"
-fi
+dpkg-query -s ns2 > /dev/null 2>&1
+case $? in
+0)
+    echo $1 has been installed successfully!
+    ;;
+1)
+    echo $1 has not been installed :(
+    ;;
+2)
+    echo An error occurred
+    ;;
+esac
 
