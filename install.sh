dcac1ab777a53ab0a4c9b9c8275515c0c08322ec
Adithya Manjunath <adithyam211@gmail.com>
2022-10-26 13:40:57 +0530

    Update install.sh

--- install.sh
+++ install.sh
@@ -15,6 +15,9 @@ echo "deb [arch=arm64,armhf,ppc64el,s390x] http://ports.ubuntu.com/ubuntu-ports/
 sudo apt update
 sudo apt-key adv --keyserver keyserver.ubuntu.com --recv-keys 3B4FE6ACC0B21F32
 sudo apt update
+sudo gpg -a --export 3B4FE6ACC0B21F32 | sudo apt-key add -
+sudo apt-key export C0B21F32 | sudo gpg --dearmour -o /usr/share/keyrings/pgdg.gpg -y
+sudo apt update
 sudo apt-get install gcc-4.8 g++-4.8 -y
 tar -xzvf ns-allinone-2.35.tar.gz -C ~/
 sudo apt-get upgrade -y
